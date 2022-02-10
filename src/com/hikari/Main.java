72c018d14f2c07c94098a2a06af9ae7abbe74fc0
Hikari-54 <climberte@yandex.ru>
2022-02-10 15:38:27 +0300

    Добавил ввод числа с клавиатуры для задачи про простые числа

--- src/com/hikari/Main.java
+++ src/com/hikari/Main.java
@@ -1,9 +1,18 @@
 package com.hikari;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        for(int i = 2; i < 100; i++) {
+    public static void main(String[] args) throws Exception  {
+        //Запрашивается ввод числа с клавиатуры, до которого (не включительно) будут проверятся все числа
+        Scanner in = new Scanner(System.in);
+        System.out.print("До какого числа вы хотите вывести все простые: ");
+        int x = in.nextInt();
+        //Основной цикл проверки, который передает все числа от 2 как аругмент
+        //в метод isPrime и получает результат - является число простым или нет
+        //если число простое - оно выводится в консоль
+        for(int i = 2; i < x; i++) {
             if (isPrime(i) == true)
                 System.out.print(i + " ");
         }
